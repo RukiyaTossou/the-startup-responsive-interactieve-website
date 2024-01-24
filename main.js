e3d0bffacf40987c2d7a331d111fcf0371d9b604
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-01-24 18:53:34 +0100

    menu nav bar

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 // quereySelectors
+// filters
 let filterButtons = document.querySelectorAll('.filters');
 let allImages = document.querySelectorAll('.ul-images-people li');
 let showAllFiltersButton = document.querySelector('.show-all-filters');
 let shortHairButton = document.querySelector('.filters-all[data-filters="short-hair"]');
 let FrontEndButton = document.querySelector('.filters-all[data-filters="Front-end"]');
-
+let Logo = document.querySelectorAll('.shake');
 let filter = 'all';
+
+// menu
+let menuButton = document.querySelector('.menu-button');
+let menu = document.querySelector('.ul-navbar');
+let menuButtonClose = document.querySelector('.colse-icon');
 
 // event listeners
 showAllFiltersButton.addEventListener('click', showAllFilters);
+shortHairButton.addEventListener('click', showFilteredImages); 
+// Logo.addEventListener('click', shake);
+menuButton.addEventListener('click', menuToggle)
 
-shortHairButton.addEventListener('click', showFilteredImages);  // Use the same function for both buttons
+// menu toggle functie
+function menuToggle(){
+    if (menu.style.display === 'block'){
+        console.log('Menu is currently visible. Hiding it.');
+        menu.style.display = 'none';
+    } else{
+        console.log('Menu is currently hidden. Showing it.');
+        menu.style.display = 'block';
+    }
+}
 
+// logo shake
+// function shake(){
+//     Logo.classList.toggle('shake');
+
+// }
 // button filters toggle
 function showAllFilters() { 
     filterButtons.forEach(button => {
@@ -45,7 +68,9 @@ function showFilteredImages(filter) {
         }
     });
 }
-        
+   
+
+
 
 // filterButtons.forEach(button => {
 //     button.addEventListener('click', function() {
